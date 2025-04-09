216b0d84e651614aed7ef614944d40f84e14dfa1
CelioJr12 <celiojunio878@gmail.com>
2025-04-09 10:59:08 -0300

    Inserir sistemas de moedas

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -4,12 +4,14 @@ public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private float hitPoints = 2;
+    [SerializeField] private int currencyWorth = 50;
 
     public void TakeDamage(float dmg){
         hitPoints -= dmg;
 
         if (hitPoints <= 0){
             EnemySpawner.onEnemyDestroy.Invoke();
+            LevelManager.main.IncreaseCurrency(currencyWorth);
             Destroy(gameObject);
         }
     }
