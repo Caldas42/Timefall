93a2a735d94314181d91bc4833a2138efbcae80d
Pedro Caldas <pedro.caldas009@gmail.com>
2025-04-22 17:06:04 -0300

    identacao melhor

--- Assets/Scripts/Plot.cs
+++ Assets/Scripts/Plot.cs
@@ -1,33 +1,33 @@
 using UnityEngine;
 
 public class Plot : MonoBehaviour{
-    [Header("References")]
-    [SerializeField] private SpriteRenderer sr; 
-    [SerializeField] private Color hoverColor;
-    private GameObject Troop;
-    private Color startColor;
+  [Header("References")]
+  [SerializeField] private SpriteRenderer sr; 
+  [SerializeField] private Color hoverColor;
+  private GameObject Troop;
+  private Color startColor;
 
-    private void Start(){
-        startColor = sr.color;
-    } 
-    private void OnMouseEnter(){
-       sr.color =  hoverColor;
-    }
-    private void OnMouseExit(){
-       sr.color = startColor;
-    }
-    private void OnMouseDown(){
-      if(Troop != null) return;
+  private void Start(){
+    startColor = sr.color;
+  } 
+  private void OnMouseEnter(){
+    sr.color =  hoverColor;
+  }
+  private void OnMouseExit(){
+    sr.color = startColor;
+  }
+  private void OnMouseDown(){
+    if(Troop != null) return;
 
     Tower TroopToBuild = BuildManager.main.GetSelectedTroop();
 
     if(TroopToBuild.cost > LevelManager.main.currency){
-      Debug.Log("YOu can't afford this tower");
+      Debug.Log("You can't afford this tower");
       return;
     }
 
     LevelManager.main.SpendCurrency(TroopToBuild.cost);
 
     Troop = Instantiate(TroopToBuild.prefab, transform.position, Quaternion.identity);
-    }
+  }
 }
