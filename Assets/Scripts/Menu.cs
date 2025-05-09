f08490ddfefb5b9403b95c53f55e1607dd967716
Joao Danilo Correia Jota <joaodanilo2006@gmail.com>
2025-05-09 12:51:51 -0300

    Menu direita

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,15 +7,6 @@ public class Menu : MonoBehaviour
     [Header("References")]
     [SerializeField] TextMeshProUGUI currencyUI;
     [SerializeField] TextMeshProUGUI remainingLivesUI;
-    [SerializeField] Animator anim;
-
-    private bool isMenuOpen = true;
-
-    public void ToggleMenu()
-    {
-        isMenuOpen = !isMenuOpen;
-        anim.SetBool("MenuOpen", isMenuOpen);
-    }
 
     void OnGUI()
     {
