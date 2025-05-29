cdaba95f6d4f5b8557c03781f9b24de2d2e35d38
tiago <tiaguinhoemilio@gmail.com>
2025-05-29 18:56:46 -0300

    ajustando tropa de fogo

--- Assets/Scripts/BurnBullet.cs
+++ Assets/Scripts/BurnBullet.cs
@@ -4,41 +4,25 @@ public class BurnBullet : Bullet
 {
     [SerializeField] private float burnDuration = 3f;
     [SerializeField] private float burnDamagePerSecond = 1f;
-    [SerializeField] private GameObject fireEffectPrefab; 
+    [SerializeField] private GameObject fireEffectPrefab;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         Health enemyHealth = other.gameObject.GetComponent<Health>();
         if (enemyHealth != null)
         {
-            StartCoroutine(ApplyBurn(enemyHealth));
+            enemyHealth.ApplyBurn(burnDamagePerSecond, burnDuration);
+
+            if (fireEffectPrefab != null)
+            {
+                GameObject fireFX = Instantiate(fireEffectPrefab, enemyHealth.transform);
+                fireFX.transform.localPosition = Vector3.zero;
+
+                Destroy(fireFX, burnDuration);
+            }
         }
 
         Destroy(gameObject);
-    }
-
-    private System.Collections.IEnumerator ApplyBurn(Health target)
-    {
-        GameObject fireFX = null;
-
-        if (fireEffectPrefab != null)
-        {
-            fireFX = Instantiate(fireEffectPrefab, target.transform);
-            fireFX.transform.localPosition = Vector3.zero;
-        }
-
-        float elapsed = 0f;
-        while (elapsed < burnDuration)
-        {
-            target.TakeDamage(burnDamagePerSecond * Time.deltaTime);
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-
-        if (fireFX != null)
-        {
-            Destroy(fireFX);
-        }
     }
 }
 
