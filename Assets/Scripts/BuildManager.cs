858673ac224fc1e80185e0b86d3ad5cee46b1fd1
João Danilo Correia Jota <joaodanilo2006@gmail.com>
2025-06-10 11:53:22 -0300

    torre nao pre selecionada

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -1,33 +1,41 @@
 using System;
 using UnityEngine;
-public class BuildManager : MonoBehaviour{
+
+public class BuildManager : MonoBehaviour
+{
     public static BuildManager main;
-    
+
     [Header("References")]
     [SerializeField] private Tower[] towers;
 
-    private int selectedtroop = 0;
+    private int selectedtroop = -1;
 
-    private void Awake() {
-        main = this; 
+    private void Awake()
+    {
+        main = this;
     }
 
-   public Tower GetSelectedTroop(){
+    public Tower GetSelectedTroop()
+    {
+        if (selectedtroop == -1)
+        {
+            return null;
+        }
         return towers[selectedtroop];
-   }  
+    }
 
-   public void SetSelectedTower(int _selectedTower) {
+    public void SetSelectedTower(int _selectedTower)
+    {
         selectedtroop = _selectedTower;
-   }
+    }
 
-     internal Tower GetTowerByIndex(int index)
-     {
-          if (index >= 0 && index < towers.Length)
-          {
-               return towers[index];
-          }
-          
-          return null;
+    internal Tower GetTowerByIndex(int index)
+    {
+        if (index >= 0 && index < towers.Length)
+        {
+            return towers[index];
+        }
+
+        return null;
     }
 }
-
