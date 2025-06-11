42503fc9d94327dbd2a597f9e80abde62390e00b
CelioJr12 <celiojunio878@gmail.com>
2025-06-11 11:45:58 -0300

    Flecha colocado e funcionado

--- Assets/Scripts/Projectiles/BasicBullet.cs
+++ Assets/Scripts/Projectiles/BasicBullet.cs
@@ -4,7 +4,7 @@ public class BasicBullet : Bullet
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
-        other.gameObject.GetComponent<Health>().TakeDamage(getBulletDamage());
+        other.gameObject.GetComponent<Health>().TakeDamage(GetBulletDamage());
         Destroy(gameObject);
     }
 }
